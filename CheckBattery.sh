e2baa8fde8ea3970cf2ee9cad96c41c8b27709fd
arthuradriaens-code <arthur.adriaens@ugent.be>
2022-11-24 12:04:07 +0100

    wrote battery check script

---
+++ CheckBattery.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+PERCENTAGE=$(cat /sys/class/power_supply/BAT0/capacity)
+LOWLIM=10
+PLUGGEDSTATE=$(cat /sys/class/power_supply/AC/online)
+ISPLUGGED=1
+
+if [ "$LOWLIM" -ge "$PERCENTAGE" ] && [ "$PLUGGEDSTATE" -ne "$ISPLUGGED" ] ; then
+	notify-send "🔋 Battery is at $PERCENTAGE %, consider charging"
+fi
