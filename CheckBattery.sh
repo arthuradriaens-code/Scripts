7fd15fdb0736cfcc6f9495b24d0888cef4d7eb62
arthuradriaens-code <arthur.adriaens@ugent.be>
2022-11-25 12:28:12 +0100

    added fully charged message

--- CheckBattery.sh
+++ CheckBattery.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 PERCENTAGE=$(cat /sys/class/power_supply/BAT0/capacity)
 LOWLIM=10
+HIGHLIM=95
 PLUGGEDSTATE=$(cat /sys/class/power_supply/AC/online)
 ISPLUGGED=1
 
 if [ "$LOWLIM" -ge "$PERCENTAGE" ] && [ "$PLUGGEDSTATE" -ne "$ISPLUGGED" ] ; then
 	notify-send "🪫 Battery is at $PERCENTAGE %, consider charging"
+fi
+
+if [ "$HIGHLIM" -le "$PERCENTAGE" ] && [ "$PLUGGEDSTATE" -eq "$ISPLUGGED" ] ; then
+	notify-send "🔋 Battery is fully charged"
 fi
