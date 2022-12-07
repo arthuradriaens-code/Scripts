65ec4ca6bf3d94fdd56713d7421380af47478864
arthuradriaens-code <arthur.adriaens@ugent.be>
2022-12-07 20:28:02 +0100

    fully functional

--- record.sh
+++ record.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-ffmpeg -video_size 1280x800 -framerate 25 -f x11grab -i :0.0+0,0 -c:v libx264rgb -crf 0 -preset ultrafast sample.mkv
+#get resolution and fps via xrandr
+ffmpeg -video_size $1 -framerate $2 -f x11grab -i :0.0 -f pulse -ac 2 -i default output.mkv
+
