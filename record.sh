49cc1eae0d4b81ad8334529e6ce460f3d752f102
arthuradriaens-code <arthur99.adriaens@gmail.com>
2022-12-19 02:49:09 +0100

    made simple movie selector

--- record.sh
+++ record.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #get resolution and fps via xrandr
-ffmpeg -video_size $1 -framerate $2 -f x11grab -i :0.0 -f pulse -ac 2 -i default output.mkv
+ffmpeg -video_size $1 -framerate $2 -f x11grab -i :0.0 -f pulse -ac 2 -i default output.mp4
 
