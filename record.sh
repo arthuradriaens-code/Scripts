007b1915fb46b52ed4ba13ef2b69f8b1158e99a2
arthuradriaens-code <arthur99.adriaens@gmail.com>
2022-12-27 17:11:26 +0100

    made help flag

--- record.sh
+++ record.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
 #get resolution and fps via xrandr
-ffmpeg -video_size $1 -framerate $2 -f x11grab -i :0.0 -f pulse -ac 2 -i default output.mp4
+#
+if [ $# -eq 1 ] && [ "$1" = "-h" ]
+  then
+    echo "\n"
+    echo "First argument: Video size, e.g 1920x1080\n"
+    echo "Second argument: framerate, e.g 60\n"
+    echo "\n"
+    exit 0
+fi
 
+ffmpeg -video_size $1 -framerate $2 -f x11grab -i :0.0 -f pulse -ac 2 -i default output.mp4
