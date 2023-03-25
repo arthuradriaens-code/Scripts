8646c3d85871198f6ad01d3088c6eb86e67e97b3
arthuradriaens-code <arthur99.adriaens@gmail.com>
2023-03-25 11:13:48 +0100

    documentation

--- getrnorun.sh
+++ getrnorun.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
-
+if [ $# -eq 1 ] && [ "$1" = "-h" ]
+  then
+    echo "\n" #White space
+    echo "first argument should be the station number, the
+    second one the run number"
+    exit 0
+fi
+if [ $# -eq 2 ]
+	then
 STATION="$1"
 RUN="$2"
 PREFIX=~/mnt/usb/RNO-G-DATA
@@ -11,5 +19,10 @@ mkdir run$RUN
 cd run$RUN
 
 curl https://rnog-data-protected.zeuthen.desy.de/inbox/station"$STATION"/run"$RUN"/combined.root -H "Authorization: Bearer `oidc-token radio`" --output combined.root
-
 cd
+exit 0
+
+fi
+
+echo "incorrect use"
+exit 1
