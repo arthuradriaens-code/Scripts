c35895bd7561770f92ad04acf4e65b66b1ed5c47
arthuradriaens-code <arthur.adriaens@ugent.be>
2023-05-05 09:42:06 +0200

    now built in a little alarm

--- CheckBattery.sh
+++ CheckBattery.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 PERCENTAGE=$(cat /sys/class/power_supply/BAT0/capacity)
 LOWLIM=10
+ALARMLIM=5
 HIGHLIM=95
 PLUGGEDSTATE=$(cat /sys/class/power_supply/AC/online)
 ISPLUGGED=1
+
+if [ "$ALARMLIM" -ge "$PERCENTAGE" ] && [ "$PLUGGEDSTATE" -ne "$ISPLUGGED" ] ; then
+	mpv extra/mixkit-alarm-tone-996.wav &
+fi
 
 if [ "$LOWLIM" -ge "$PERCENTAGE" ] && [ "$PLUGGEDSTATE" -ne "$ISPLUGGED" ] ; then
 	notify-send "🪫 Battery is at $PERCENTAGE %, consider charging"
